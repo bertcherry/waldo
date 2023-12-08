2405105c925fe34b8053bddf43f79b0d98b6ab64
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-12-08 11:35:17 -0800

    add answer key data

---
+++ api/assets/deathstarkey.js
@@ -0,0 +1,27 @@
+const answers = [
+    {
+        name: 'Han Solo',
+        x: .435,
+        y: .970,
+    }, {
+        name: 'C3PO & R2D2',
+        x: .939,
+        y: .935,
+    },{
+        name: 'Luke Skywalker with Lightsaber',
+        x: .869,
+        y: .441,
+    },{
+        name: 'Chewbacca',
+        x: .706,
+        y: .981,
+    },{
+        name: 'Imperial Probe Droid',
+        x: .480,
+        y: .465,
+    },{
+        name: 'Imperial Shuttle',
+        x: .233,
+        y: .384,
+    }
+];
